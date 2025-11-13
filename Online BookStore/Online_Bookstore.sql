3c5415fa45e17b1f9f0fdc1dad6a458dd7aec225
riyatanoj30 <riyatmhatre@gmail.com>
2025-11-13 12:34:20 +0530

    Update CSV file paths for data import

--- Online BookStore/Online_Bookstore.sql
+++ Online BookStore/Online_Bookstore.sql
@@ -40,17 +40,17 @@ SELECT * FROM Orders;
 
 -- Import Data into Books Table
 COPY Books(Book_ID, Title, Author, Genre, Published_Year, Price, Stock) 
-FROM 'D:\Course Updates\30 Day Series\SQL\CSV\Books.csv' 
+FROM 'D:\Major Projects\MySQL-Projects\Online BookStore\Books.csv' 
 CSV HEADER;
 
 -- Import Data into Customers Table
 COPY Customers(Customer_ID, Name, Email, Phone, City, Country) 
-FROM 'D:\Course Updates\30 Day Series\SQL\CSV\Customers.csv' 
+FROM 'D:\Major Projects\MySQL-Projects\Online BookStore\Customers.csv' 
 CSV HEADER;
 
 -- Import Data into Orders Table
 COPY Orders(Order_ID, Customer_ID, Book_ID, Order_Date, Quantity, Total_Amount) 
-FROM 'D:\Course Updates\30 Day Series\SQL\CSV\Orders.csv' 
+FROM 'D:\Major Projects\MySQL-Projects\Online BookStore\Orders.csv' 
 CSV HEADER;
 
 -- Basic Questions
